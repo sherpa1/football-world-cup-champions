7b129d7341c1dc27bae4ebf52311a6b6870535e8
Alexandre Leroux <alex@sherpa.one>
2020-04-16 08:19:23 +0200

    documentation firebase / firestore

--- src/rename-to-firebase.js
+++ src/rename-to-firebase.js
@@ -1,4 +1,6 @@
-//Rename to firebase.js
+//Cliquer sur l'icône rouage en haut à gauche de l'interface "Paramètres / Settings" -> "Vos Applications"
+//Récupérer les accès à votre base de données firestore, au préalable complétée en suivant le modèle de données décrit dans le fichier /readme.md
+//Renommer ce fichier en firebase.js
 
 /*
 import { firebase } from "@firebase/app";
